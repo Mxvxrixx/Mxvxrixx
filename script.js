90edcc044230e8ff0a9f8db3ec163c9dea94671a
Mxvxrixx <natthapon.guitar@gmail.com>
2024-08-15 11:44:17 +0700

    𝐌𝐱𝐯𝐱𝐫𝐢𝐱𝐱

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-// ไม่มีการใช้งาน JavaScript ในไฟล์นี้
-// ไฟล์นี้สามารถใช้สำหรับการเพิ่มฟังก์ชัน JavaScript ในอนาคตได้
+// JavaScript สำหรับการจัดการกับรูปโปรไฟล์และ Modal
+
+// รับรูปโปรไฟล์และ Modal
+var profilePic = document.getElementById('profilePic');
+var modal = document.getElementById('modal');
+var imgModal = document.getElementById('imgModal');
+var closeModal = document.getElementById('closeModal');
+
+// เมื่อคลิกรูปโปรไฟล์
+profilePic.onclick = function() {
+    modal.style.display = "block";
+    imgModal.src = this.src;
+}
+
+// เมื่อคลิกปุ่มปิด (x)
+closeModal.onclick = function() {
+    modal.style.display = "none";
+}
+
+// เมื่อคลิกที่นอก Modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
